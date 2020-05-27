e27fb48d0810083c8e3758251586faf4d6b2e103
Robert Lee <32447910+robertklee@users.noreply.github.com>
2020-05-26 22:23:34 -0700

    added sample C++ file

--- main.cpp
+++ main.cpp
@@ -1,8 +1,49 @@
+// File name: ~ftp/pub/class/cplusplus/Pointer/PointerSimple.cpp
+// Credit to: ftp://www.cs.uregina.ca/pub/class/cplusplus/Pointer.html
+// Purpose:   A simple program demonstrating the use of pointers.
+
 #include <iostream>
+using namespace std;
 
-int main(int argc, char const *argv[])
+int main()
 {
-    std::cout << "Welcome to IEEE skillDev Summer 2020!" << std::endl;
+	// declare an integer and a float variable
+	int IntNum; 
+	float FloatNum;
+	
+	// declare integer and float pointers
+	int *pIntNum;
+	float *pFloatNum;
+	
+	// initialize the integer and float variables
+	IntNum = 10;
+	FloatNum = 12.34;
+	
+	// store addresses in pointers
+	pIntNum = &IntNum;
+	pFloatNum = &FloatNum;
 
-    return 0;
+	// print out the original values
+        cout << "Before increment: " << endl;
+	cout << "\t IntNum is: " << IntNum << endl;
+	cout << "\t FloatNum is: " << FloatNum << endl;
+	
+	// note that we need to dereference a pointer in order
+	// to extract the value it contains.
+	cout << "\t pIntNum contains: " << *pIntNum << endl;
+	cout << "\t pFloatNum contains: " << *pFloatNum << endl;
+
+	// increment values of the integer and float variables 
+	(*pIntNum)++;  // dereference and then increment
+	(*pFloatNum)++;
+
+	// print out the values after increment
+        cout << "After increment: " << endl;
+	cout << "\t IntNum is: " << IntNum << endl;
+	cout << "\t FloatNum is: " << FloatNum << endl;
+
+	cout << "\t pIntNum contains: " << *pIntNum << endl;
+	cout << "\t pFloatNum contains: " << *pFloatNum << endl;
+
+	return 0;
 }
